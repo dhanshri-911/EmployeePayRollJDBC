afc73bafb3e5f29d8c7e11ad3a99a1046f0e80c9
Dhanshri911 <dszingade911@gmail.com>
2022-04-27 01:26:09 +0530

    UC4_Added Search By Name

--- src/com/bridgelabz/DBDemo.java
+++ src/com/bridgelabz/DBDemo.java
@@ -4,11 +4,11 @@ import java.sql.*;
 import java.util.Enumeration;
 
 public class DBDemo {
+    static Connection con = null;
     public static void main(String[] args) throws SQLException {
         String jdbcURL = "jdbc:mysql://localhost:3306/Payroll_Service?useSSL=false";
         String userName = "root";
         String password = "admin00";
-        Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver loaded");
@@ -29,16 +29,35 @@ public class DBDemo {
         ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
         System.out.println("Before Update");
         display(resultSet);
+        System.out.println("");
         System.out.println("After Update");
-        String updatequery = "update employee_payroll set Basic_Pay = ? where name = ?";
+        String updatequery = "update employee_payroll set Basic_pay = ? where name = ?";
+        updateQuery(updatequery);
+        System.out.println("+****");
+        String search = "select * from employee_payroll where name =?";
+        retriveBYName(search);
+    }
+
+    private static void retriveBYName(String search) throws SQLException {
+        PreparedStatement preparedStatement = con.prepareStatement(search);
+        preparedStatement.setString(1, "Terisa");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            System.out.println(resultSet.getString(1)+","+resultSet.getString(2)+","+resultSet.getString(3)+","+resultSet.getString(4)+","+resultSet.getString(5));
+        }
+        else System.out.println("Record not Found:");
+    }
+
+    public static String updateQuery(String updatequery) throws SQLException {
         PreparedStatement preparedStatement = con.prepareStatement(updatequery);
         preparedStatement.setInt(1,3000000);
-        preparedStatement.setString(2,"Terisa");
+        preparedStatement.setString(2,"Teresa");
         preparedStatement.executeUpdate();
-        String updatedquery = "select * from employee_payroll where name = ?";
+        String updatedquery = "select * from employee_payroll where Employee_Name = ?";
         PreparedStatement preparedStatement1 = con.prepareStatement(updatedquery);
         ResultSet resultSet1 = preparedStatement1.executeQuery("select * from employee_payroll");
         display(resultSet1);
+        return updatedquery;
     }
     static void listDrivers() {
         Enumeration<Driver> driverList = DriverManager.getDrivers();
